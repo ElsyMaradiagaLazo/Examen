d2cc0f6c25ac672c8cc01eaa25cd6e4ed559c083
ElsyMaradiagaLazo <98985096+ElsyMaradiagaLazo@users.noreply.github.com>
2022-04-25 18:44:58 -0600

    Examen
    Login

---
+++ ExamenLenguaje3/Examen/Examen/Pages/Usuario/Usuarios.razor.cs
@@ -0,0 +1,16 @@
+﻿using Examen.Interfaces;
+using Microsoft.AspNetCore.Components;
+using Modelos;
+
+namespace Examen.Pages.Usuarios;
+
+partial class Usuarios
+{
+    [Inject] private IUsuarioServicio _usuarioServicio { get; set; }
+
+    private IEnumerable<Usuario> usuariosLista { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        usuariosLista = await _usuarioServicio.GetLista();
+    }
